f62575ff6611bc601157df0ab847154fa8e4bfa5
Evgeniy Moiseenko <evg.moiseenko@mail.ru>
2015-08-17 22:20:46 +0300

    suffix tree implementation

--- task5/suff_tree.c
+++ task5/suff_tree.c
@@ -2,6 +2,7 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 static const char START_ASCII_CODE  = 32;
 static const char END_ASCII_CODE    = 127;
@@ -28,28 +29,15 @@ void st_init_node(st_node* node, int32_t left, int32_t right, int32_t parent)
     node->m_left        = left;
     node->m_right       = right;
     node->m_parent      = parent;
-    node->m_children    = (int32_t*) malloc(sizeof(int32_t) * (END_ASCII_CODE - START_ASCII_CODE));
     node->m_suflink     = -1;
-}
 
-st_tree* st_create_tree(int32_t size)
-{
-    st_tree* tree = (st_tree*) malloc(sizeof(st_tree));
-    tree->m_nodes = (st_node*) malloc(2 * size * sizeof(st_node));
-    tree->m_size  = 1;
-    st_init_node(&tree->m_nodes[0], 0, 0, -1);
-    return tree;
+    node->m_children    = (int32_t*) malloc(sizeof(int32_t) * (END_ASCII_CODE - START_ASCII_CODE));
+    memset((void*) node->m_children, -1, sizeof(int32_t) * (END_ASCII_CODE - START_ASCII_CODE));
 }
 
 // append new node to the suffix tree
 int32_t st_append_node(st_tree* tree, int32_t left, int32_t right, int32_t parent)
 {
-//    if (tree->m_size == tree->m_capacity) {
-//        st_node* newmem = (st_node*) malloc(2 * tree->m_size * sizeof(st_node));
-//        memcpy((void*) newmem, (void*) tree->m_nodes, tree->m_size * sizeof(st_node));
-//        free(tree->m_nodes);
-//        tree->m_nodes = newmem;
-//    }
     int32_t ind = tree->m_size++;
     st_init_node(&tree->m_nodes[ind], left, right, parent);
     return ind;
@@ -112,7 +100,7 @@ int32_t st_split(const char* str, st_tree* tree, st_state state)
     st_node* new_node = &tree->m_nodes[newni];
 
     st_set_children(parent, lch, newni);
-    st_set_children(new_node, pch, node);
+    st_set_children(new_node, pch, state.m_node);
     node->m_parent = newni;
     node->m_left = piv;
 
@@ -125,27 +113,27 @@ int32_t st_suflink(const char* str, st_tree* tree, st_node* node)
     if (node->m_suflink != -1) {
         return node->m_suflink;
     }
-    if (node->m_parent == 0) {
+    if (node->m_parent == -1) {
         return 0;
     }
 
     int32_t plink = st_suflink(str, tree, &tree->m_nodes[node->m_parent]);
     st_state state;
     state.m_node = plink;
-    state.m_pos = st_suflen(&tree->m_nodes[plink]);
+    state.m_pos  = st_suflen(&tree->m_nodes[plink]);
 
     int32_t left = node->m_left;
     int32_t right = node->m_right;
     if (node->m_parent == 0) {
         ++left;
     }
-    state tr = st_transition(str, tree, state, left, right);
+    st_state tr = st_transition(str, tree, state, left, right);
     node->m_suflink = st_split(str, tree, tr);
     return node->m_suflink;
 }
 
 // extend suffix tree corresponded to str from [0 ... pos] to [0 ... pos+1]
-state st_extend_tree(const char* str, int32_t len, st_tree* tree, st_state state, int32_t pos)
+st_state st_extend_tree(const char* str, int32_t len, st_tree* tree, st_state state, int32_t pos)
 {
     while(true) {
         st_state newst = st_transition(str, tree, state, pos, pos + 1);
@@ -163,4 +151,31 @@ state st_extend_tree(const char* str, int32_t len, st_tree* tree, st_state state
             return state;
         }
     }
+}
+
+st_tree* st_create_tree(const char* str, int32_t len)
+{
+    st_tree* tree = (st_tree*) malloc(sizeof(st_tree));
+    tree->m_nodes = (st_node*) malloc(2 * len * sizeof(st_node));
+    tree->m_size  = 1;
+    // init root
+    st_init_node(&tree->m_nodes[0], 0, 0, -1);
+    // build suffix tree
+    st_state state;
+    state.m_node = 0;
+    state.m_pos  = 0;
+    for (int32_t i = 0; i < len; ++i) {
+        state = st_extend_tree(str, len, tree, state, i);
+    }
+
+    return tree;
+}
+
+void st_destroy_tree(st_tree* tree)
+{
+    for (int32_t i = 0; i < tree->m_size; ++i) {
+        free((void*) tree->m_nodes[i].m_children);
+    }
+    free((void*) tree->m_nodes);
+    free((void*) tree);
 }
--- task5/suff_tree.h
+++ task5/suff_tree.h
@@ -5,27 +5,31 @@
 
 // NOTE: we use int32_t everywhere because this type is large enough to index 128 Mb == 2^27 byte.
 
-struct st_node
+typedef struct st_node
 {
     int32_t     m_left;
     int32_t     m_right;
     int32_t     m_parent;
     int32_t     m_suflink;
     int32_t*    m_children;
-};
+} st_node;
 
-struct st_state
+typedef struct st_state
 {
     int32_t     m_node;
     int32_t     m_pos;
-};
+} st_state;
 
-struct st_tree
+typedef struct st_tree
 {
     st_node*    m_nodes;
     int32_t     m_size;
     int32_t     m_capacity;
-};
+} st_tree;
+
+
+st_tree* st_create_tree(const char* str, int32_t len);
+void st_destroy_tree(st_tree* tree);
 
 
 #endif // SUFF_TREE_H
